74671abbd17dbd005e4cad6155bcc7a3081f6bce
Aidan Shanley <ashanley@csumb.edu>
2026-01-27 22:35:33 -0800

    Add files via upload
    Made a login page. Hooray!

---
+++ app/src/main/java/com/example/project1cst438/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.project1cst438.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
