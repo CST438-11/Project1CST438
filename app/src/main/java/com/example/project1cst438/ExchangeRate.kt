5f674966cbf4b71d189ad7d2c5390e1b97ab6d2a
Brandon Algarra <balgarra@csumb.edu>
2026-02-14 18:08:44 -0800

    Added dropdown to take user input and query with new base currency. Added button to save as user preference // TODO: wire button to save to user table

--- app/src/main/java/com/example/project1cst438/ExchangeRate.kt
+++ app/src/main/java/com/example/project1cst438/ExchangeRate.kt
@@ -5,19 +5,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -49,92 +41,137 @@ class Display : ComponentActivity() {
 @Composable
 fun ExchangeRateScreen(
     modifier: Modifier = Modifier,
-    viewModel: ExchangeRateViewModel = viewModel()) {
+    viewModel: ExchangeRateViewModel = viewModel()
+) {
 
     val rates = viewModel.rates
+    val currencyOptions = rates?.conversionRates
+        ?.keys
+        ?.toList()
+        ?: emptyList()
     val error = viewModel.errorMessage
     val context = LocalContext.current
-    Row(modifier = modifier.fillMaxWidth()) {
-        Modifier
-        Button(
-            onClick = {
-                context.startActivity(
-                    Intent(context, MainActivity::class.java)
-                )
-            },
-            modifier = Modifier
-//                .padding(bottom = 40.dp)
-                .padding(top = 10.dp)
-                .fillMaxWidth(0.5f)
-        ) {
-            Text("Back to Home")
+    var expanded by remember { mutableStateOf(false) }
+    Column(modifier = modifier.fillMaxSize()) {
+        Row(modifier = Modifier.fillMaxWidth()
+            .padding(horizontal = 2.dp)) {
+            Button(
+                onClick = {
+                    context.startActivity(
+                        Intent(context, MainActivity::class.java)
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(0.5f)
+            ) {
+                Text("Back to Home")
+            }
+
+            Button(
+                onClick = {
+
+
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Pair conversion")
+            }
         }
-
-        Button(
-            onClick = {
-
-
-            },
+        Row(
             modifier = Modifier
-//                .padding(bottom = 40.dp)
-                .padding(top = 10.dp)
                 .fillMaxWidth()
+                .padding(horizontal = 2.dp)
         ) {
-            Text("Pair conversion")
-        }
-    }
-    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        when {
-            error != null -> Text("Error: $error")
-            rates == null -> CircularProgressIndicator()
-            else -> {
-                // Map -> sorted list so order is stable
-                val ratesList = rates.conversionRates
-                    .toList()               // List<Pair<String, Double>>
-                    .sortedBy { it.first }  // sort by currency code
+            Box(modifier = Modifier.weight(1f)) {
 
-                LazyColumn(
+                Button(
+                    onClick = { expanded = true },
                     modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp)
-                        .padding(top = 60.dp)
+                        .fillMaxWidth()
                 ) {
-                    // Header row
-                    item {
-                        Text("Base: ${rates.baseCode}")
-                        Divider(modifier = Modifier.padding(vertical = 12.dp))
-                    }
+                    Text("Base currency: ${viewModel.baseCurrency}")
+                }
 
-                    items(ratesList) { (code, rate) ->
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(vertical = 8.dp),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            // For each Row of data, display:
-                            // Base currency (USD)
-                            Text(
-                                text = rates.baseCode,
-                                modifier = Modifier.padding(end = 8.dp)
-                            )
-                            // Arrow icon
-                            Icon(
-                                imageVector = Icons.Default.ArrowForward,
-                                contentDescription = "to",
-                                modifier = Modifier.padding(end = 8.dp)
-                            )
-                            // Conversion currency
-                            Text(
-                                text = "$code :",
-                                modifier = Modifier.weight(1f)
-                            )
-                            // Rate aligned to right
-                            Text(
-                                text = String.format("%.4f", rate)
-                            )
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    currencyOptions.forEach { currency ->
+                        DropdownMenuItem(
+                            text = { Text(currency) },
+                            onClick = {
+                                expanded = false
+                                viewModel.changeBaseCurrency(currency)
+                            }
+                        )
+                    }
+                }
+            }
+
+            Button(
+                onClick = {
+
+
+                },
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text("Save as default")
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            when {
+                error != null -> Text("Error: $error")
+                rates == null -> CircularProgressIndicator()
+                else -> {
+                    // Map -> sorted list so order is stable
+                    val ratesList = rates.conversionRates
+                        .toList()               // List<Pair<String, Double>>
+                        .sortedBy { it.first }  // sort by currency code
+
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp)
+                            .padding(top = 10.dp)
+                    ) {
+
+                        items(ratesList) { (code, rate) ->
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 8.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                // For each Row of data, display:
+                                // Base currency (USD)
+                                Text(
+                                    text = rates.baseCode,
+                                    modifier = Modifier.padding(end = 8.dp)
+                                )
+                                // Arrow icon
+                                Icon(
+                                    imageVector = Icons.Default.ArrowForward,
+                                    contentDescription = "to",
+                                    modifier = Modifier.padding(end = 8.dp)
+                                )
+                                // Conversion currency
+                                Text(
+                                    text = "$code :",
+                                    modifier = Modifier.weight(1f)
+                                )
+                                // Rate aligned to right
+                                Text(
+                                    text = String.format("%.4f", rate)
+                                )
+                            }
+                            Divider()
                         }
-                        Divider()
                     }
                 }
             }
